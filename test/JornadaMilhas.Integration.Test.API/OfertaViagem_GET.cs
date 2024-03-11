1982bd1b7d3141a90a8a64215d3a0e24dfb1f2fb
André Santana <andrelms4@insper.edu.br>
2024-03-11 15:44:15 -0300

    #Vídeo3.1

---
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class OfertaViagem_GET : IClassFixture<JornadaMilhasWebApplicationFactory>
+    {
+        private readonly JornadaMilhasWebApplicationFactory app;
+        public OfertaViagem_GET(JornadaMilhasWebApplicationFactory app)
+        {
+            this.app = app;
+        }
+
+        [Fact]
+        public async Task Recupera_OfertaViagem_PorId()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
