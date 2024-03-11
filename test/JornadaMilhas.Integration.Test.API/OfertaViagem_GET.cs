90344e854ab51931ecb4869672a5d52595b868d4
André Santana <andrelms4@insper.edu.br>
2024-03-11 15:58:13 -0300

    #Video 3.2

--- test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,8 +21,30 @@ namespace JornadaMilhas.Integration.Test.API
         public async Task Recupera_OfertaViagem_PorId()
         {
             //Arrange
+            var ofertaExistente = app.Context.OfertasViagem.FirstOrDefault();
+            if (ofertaExistente is null)
+            {
+                ofertaExistente = new OfertaViagem()
+                {
+                    Preco = 100,
+                    Rota = new Rota("Origem", "Destino"),
+                    Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+                };
+                app.Context.Add(ofertaExistente);
+                app.Context.SaveChanges();
+            }
+
+            using var client = await app.GetClientWithAccessTokenAsync();
+
             //Act
+            var response = await client.GetFromJsonAsync<OfertaViagem>("/ofertas-viagem/" + ofertaExistente.Id);
+
             //Assert
+            Assert.NotNull(response);
+            Assert.Equal(ofertaExistente.Preco, response.Preco, 0.001);
+            Assert.Equal(ofertaExistente.Rota.Origem, response.Rota.Origem);
+            Assert.Equal(ofertaExistente.Rota.Destino, response.Rota.Destino);
+
         }
     }
 }
