9f679409f1a8d81e52164754f1c6c8703135156d
André Santana <andrelms4@insper.edu.br>
2024-03-11 14:35:27 -0300

    #update: Vídeo 2.1

---
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
@@ -0,0 +1,37 @@
+﻿using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class OfertaViagem_POST
+    {
+        [Fact]
+        public async Task Cadastra_OfertaViagem()
+        {
+            //Arrange
+            var app = new JornadaMilhasWebApplicationFactory();
+
+            using var client = app.CreateClient();
+
+            var ofertaViagem = new OfertaViagem()
+            {
+                Preco = 100,
+                Rota = new Rota("Origem", "Destino"),
+                Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+            };
+            //Act
+            var response = await client.PostAsJsonAsync("/ofertas-viagem", ofertaViagem);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
