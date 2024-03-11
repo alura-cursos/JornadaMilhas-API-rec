52dca1a919c0095a56c1a41ec221f003f12ce098
André Santana <andrelms4@insper.edu.br>
2024-03-11 15:24:49 -0300

    #update: Ajustes.

--- test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
@@ -10,13 +10,38 @@ using System.Threading.Tasks;
 
 namespace JornadaMilhas.Integration.Test.API
 {
-    public class OfertaViagem_POST
+    public class OfertaViagem_POST:IClassFixture<JornadaMilhasWebApplicationFactory>
     {
+        private readonly JornadaMilhasWebApplicationFactory app;
+
+        public OfertaViagem_POST(JornadaMilhasWebApplicationFactory app)
+        {
+            this.app = app;
+        }
+
         [Fact]
         public async Task Cadastra_OfertaViagem()
         {
             //Arrange
-            var app = new JornadaMilhasWebApplicationFactory();
+           using var client = await app.GetClientWithAccessTokenAsync();
+
+            var ofertaViagem = new OfertaViagem()
+            {
+                Preco = 100,
+                Rota = new Rota("Origem", "Destino"),
+                Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+            };
+            //Act
+            var response = await client.PostAsJsonAsync("/ofertas-viagem", ofertaViagem);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Cadastra_OfertaViagem_SemAutorizacao()
+        {
+            //Arrange   
 
             using var client = app.CreateClient();
 
@@ -30,7 +55,7 @@ namespace JornadaMilhas.Integration.Test.API
             var response = await client.PostAsJsonAsync("/ofertas-viagem", ofertaViagem);
 
             //Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
     }
