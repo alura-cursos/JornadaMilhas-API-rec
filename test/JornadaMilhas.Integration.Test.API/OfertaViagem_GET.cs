07a7aa23734e50e1d74ef71d4bbae21fc79ccf1b
André Santana <andrelms4@insper.edu.br>
2024-03-12 11:08:10 -0300

    #Vídeo 4.1

--- test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
@@ -1,50 +1,68 @@
 ﻿using JornadaMilhas.Dominio.Entidades;
 using JornadaMilhas.Dominio.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using JornadaMilhas.Integration.Test.API.DataBuilders;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace JornadaMilhas.Integration.Test.API
+namespace JornadaMilhas.Integration.Test.API;
+
+public class OfertaViagem_GET : IClassFixture<JornadaMilhasWebApplicationFactory>
 {
-    public class OfertaViagem_GET : IClassFixture<JornadaMilhasWebApplicationFactory>
+    private readonly JornadaMilhasWebApplicationFactory app;
+    public OfertaViagem_GET(JornadaMilhasWebApplicationFactory app)
     {
-        private readonly JornadaMilhasWebApplicationFactory app;
-        public OfertaViagem_GET(JornadaMilhasWebApplicationFactory app)
-        {
-            this.app = app;
-        }
+        this.app = app;
+    }
 
-        [Fact]
-        public async Task Recupera_OfertaViagem_PorId()
+    [Fact]
+    public async Task Recupera_OfertaViagem_PorId()
+    {
+        //Arrange
+        var ofertaExistente = app.Context.OfertasViagem.FirstOrDefault();
+        if (ofertaExistente is null)
         {
-            //Arrange
-            var ofertaExistente = app.Context.OfertasViagem.FirstOrDefault();
-            if (ofertaExistente is null)
+            ofertaExistente = new OfertaViagem()
             {
-                ofertaExistente = new OfertaViagem()
-                {
-                    Preco = 100,
-                    Rota = new Rota("Origem", "Destino"),
-                    Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
-                };
-                app.Context.Add(ofertaExistente);
-                app.Context.SaveChanges();
-            }
-
-            using var client = await app.GetClientWithAccessTokenAsync();
-
-            //Act
-            var response = await client.GetFromJsonAsync<OfertaViagem>("/ofertas-viagem/" + ofertaExistente.Id);
-
-            //Assert
-            Assert.NotNull(response);
-            Assert.Equal(ofertaExistente.Preco, response.Preco, 0.001);
-            Assert.Equal(ofertaExistente.Rota.Origem, response.Rota.Origem);
-            Assert.Equal(ofertaExistente.Rota.Destino, response.Rota.Destino);
-
+                Preco = 100,
+                Rota = new Rota("Origem", "Destino"),
+                Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+            };
+            app.Context.Add(ofertaExistente);
+            app.Context.SaveChanges();
         }
+
+        using var client = await app.GetClientWithAccessTokenAsync();
+
+        //Act
+        var response = await client.GetFromJsonAsync<OfertaViagem>("/ofertas-viagem/" + ofertaExistente.Id);
+
+        //Assert
+        Assert.NotNull(response);
+        Assert.Equal(ofertaExistente.Preco, response.Preco, 0.001);
+        Assert.Equal(ofertaExistente.Rota.Origem, response.Rota.Origem);
+        Assert.Equal(ofertaExistente.Rota.Destino, response.Rota.Destino);
+
+    }
+
+    [Fact]
+    public async Task Recuperar_OfertasViagens_Na_Consulta_Paginada()
+    {
+        //Arrange
+        var ofertaDataBuilder = new OfertaViagemDataBuilder();
+        var listaDeOfertas = ofertaDataBuilder.Generate(80);
+        app.Context.OfertasViagem.AddRange(listaDeOfertas);
+        app.Context.SaveChanges();
+
+        using var client = await app.GetClientWithAccessTokenAsync();
+
+        int pagina = 1;
+        int tamanhoPorPagina = 80;
+
+        //Act
+        var response = await client.GetFromJsonAsync<ICollection<OfertaViagem>>($"/ofertas-viagem?pagina={pagina}&tamanhoPorPagina={tamanhoPorPagina}");
+
+        //Assert
+        Assert.True(response != null);
+        Assert.Equal(tamanhoPorPagina, response.Count());
+
     }
 }
--- test/JornadaMilhas.Integration.Test.API/OfertaViagem_PUT.cs
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_PUT.cs
@@ -1,12 +1,7 @@
 ﻿using JornadaMilhas.Dominio.Entidades;
 using JornadaMilhas.Dominio.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JornadaMilhas.Integration.Test.API
 {
