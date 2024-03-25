720de6c5a65719366af0912622e8b1ffb60f2586
bessax <andrebessax@gmail.com>
2024-03-25 11:16:43 -0300

    #Update: Implementação mão na massa 3.3, testes de integração para os endpoints de rota.

---
+++ test/JornadaMilhas.Integration.Test.API/Rota_DELETE.cs
@@ -0,0 +1,49 @@
+﻿using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class Rota_DELETE : IClassFixture<JornadaMilhasWebApplicationFactory>
+    {
+        private readonly JornadaMilhasWebApplicationFactory app;
+
+        public Rota_DELETE(JornadaMilhasWebApplicationFactory app)
+        {
+            this.app = app;
+        }
+
+        [Fact]
+        public async Task Deletar_Rota_PorId() {
+            //Arrange
+            var rotaExistente = app.Context.Rota.FirstOrDefault();
+            if (rotaExistente is null)
+            {
+                rotaExistente = new Rota()
+                {
+                    Origem = "Origem",
+                    Destino = "Destino"
+                };
+                app.Context.Add(rotaExistente);
+                app.Context.SaveChanges();
+            }
+
+            using var client = await app.GetClientWithAccessTokenAsync();
+
+            //Act
+            var response = await client.DeleteAsync("/rota-viagem/" + rotaExistente.Id);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+        }
+
+
+    }
+}
---
+++ test/JornadaMilhas.Integration.Test.API/Rota_POST.cs
@@ -0,0 +1,54 @@
+﻿using JornadaMilhas.Dominio.Entidades;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class Rota_POST:IClassFixture<JornadaMilhasWebApplicationFactory>
+    {
+        private readonly JornadaMilhasWebApplicationFactory app;
+
+        public Rota_POST(JornadaMilhasWebApplicationFactory app)
+        {
+            this.app = app;
+        }
+
+        [Fact]
+        public async Task Cadastra_Rota()
+        {
+            //Arrange
+           using var client = await app.GetClientWithAccessTokenAsync();
+
+            var rotaViagem = new Rota()
+            {
+              Origem = "Origem",
+              Destino = "Destino"
+            };
+            //Act
+            var response = await client.PostAsJsonAsync("/rota-viagem", rotaViagem);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Cadastra_Rota_SemAutorizacao()
+        {
+            //Arrange   
+
+            using var client = app.CreateClient();
+
+            var rotaViagem = new Rota()
+            {
+                Origem = "Origem",
+                Destino = "Destino"
+            };
+            //Act
+            var response = await client.PostAsJsonAsync("/rota-viagem", rotaViagem);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+    }
+}
